74573aee28ec5b23e3e0e8737c7591d7cb89e8bf
Sara Carpinone <carp0052@algonquinlive.com>
2014-12-10 15:11:51 -0500

    update app

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var shopping_carp0052 = {
+    shoppingList: [],
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -27,25 +29,83 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        //document.addEventListener("DOMContentLoaded", this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        shopping_carp0052.receivedEvent('deviceready');
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+          
+          if(localStorage.getItem("shopping-carp0052")){
+            shopping_carp0052.shoppingList = JSON.parse(localStorage.getItem("shopping-carp0052"));
+           // console.log("exists");
+            shopping_carp0052.showList();
+          }else{
+            console.log("empty");
+          }
+         var btn= document.querySelector("#addItem");    
+         btn.addEventListener("click", shopping_carp0052.addItem );
+         
+    },
+    
+    addItem: function(ev){
+                ev.preventDefault();
+                
+                var newItem = document.querySelector("#newItem").value;  
+                if (newItem == null || newItem == "") {        
+                    return false;
+                }                
+                shopping_carp0052.shoppingList.push(newItem);                
+                localStorage.setItem("shopping-carp0052", JSON.stringify(shopping_carp0052.shoppingList) );
+                shopping_carp0052.showList();
+                $("input:text").val("");
+                
+                return false;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    },
+    
+    removeItem:function (ev){
+                
+                var txt = ev.currentTarget.firstChild.nodeValue;        
+                for(var i=0;i<shopping_carp0052.shoppingList.length;i++){
+                  if(shopping_carp0052.shoppingList[i] == txt){
+                      shopping_carp0052.shoppingList.splice(i, 1);               
+                      localStorage.removeItem(shopping_carp0052.item);
+                  }
+                }
+            localStorage.setItem("shopping-carp0052", JSON.stringify(shopping_carp0052.shoppingList) );
+            shopping_carp0052.showList();
+ },
+    
+    showList: function(){            
+                var list = document.querySelector("#addedItem");
+                list.innerHTML = "";
+            
+            for(var i=0;i<shopping_carp0052.shoppingList.length;i++){
+               // var remove = document.createElement("span");
+                //remove.setAttribute("class", "ui-btn ui-icon-minus ui-btn-icon-notext ui-corner-all");
+                    
+                        
+                
+                var item = document.createElement("li");
+                item.innerHTML = shopping_carp0052.shoppingList[i];    
+                
+                
+                list.appendChild(item);
+               // item.appendChild(remove);
+                
+                $(list).listview('refresh');
+                
+                
+                item.addEventListener("click", shopping_carp0052.removeItem);
+          }       
     }
 };
 
-app.initialize();
+shopping_carp0052.initialize();
